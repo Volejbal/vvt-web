37b0cffa9723973a7ff8c17f08e0f6c5b77aefd7
Volejbal <dlouha.martin@gmail.com>
2026-07-08 19:42:18 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Veterinární volejbalový turnaj
+// Základní soubor pro budoucí funkce webu
+
+console.log("Web Veterinárního volejbalového turnaje je připraven.");
+
+// Sem později přidáme:
+// - mobilní menu
+// - galerii s otevřením fotografií
+// - animace při scrollování
+// - načítání výsledků
+// - další interaktivní prvky
